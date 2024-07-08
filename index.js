152e223ddec3145f5c0fbf8f7d59857532b887dc
RelicR <99069626+RelicR@users.noreply.github.com>
2024-07-09 01:49:58 +0400

    Update index.js

--- index.js
+++ index.js
@@ -249,6 +249,20 @@ const lastInter = {};
 const currDate = new Date();
 var prevMsg;
 
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`https://relic-ext.onrender.com/`);
+}, 280000);
+
+
 global.client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
 
 client.commands = new Collection();
