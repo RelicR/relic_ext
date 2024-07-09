971fa2068b0a7d47307fbbeef1892774f98000e0
RelicR <99069626+RelicR@users.noreply.github.com>
2024-07-09 23:22:41 +0400

    Update index.js

--- index.js
+++ index.js
@@ -462,17 +462,17 @@ app.listen(process.env.PORT);
 //   http.get(`http://127.0.0.1:${process.env.PORT}/`);
 // }, 10000);
 
-setInterval(() => {
-	fetch(`https://relic-ext.onrender.com/`, {
-		method: "get",
-		headers: {
-			"Host": `relic-ext.onrender.com`,
-			"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)",
-			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-			"Accept-Language": "en-us,en;q=0.5",
-		}
-	});
-  }, 280000);
+//setInterval(() => {
+//	fetch(`https://relic-ext.onrender.com/`, {
+//		method: "get",
+//		headers: {
+//			"Host": `relic-ext.onrender.com`,
+//			"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)",
+//			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+//			"Accept-Language": "en-us,en;q=0.5",
+//		}
+//	});
+//  }, 280000);
 
 
 
