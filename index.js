2514cc5b8bb1a0c9e705c73fc3c3f8904c4bf5dd
RelicR <99069626+RelicR@users.noreply.github.com>
2024-07-09 12:48:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -249,13 +249,6 @@ const lastInter = {};
 const currDate = new Date();
 var prevMsg;
 
-
-const http = require('http');
-const express = require('express');
-const app = express();
-
-
-
 global.client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
 
 client.commands = new Collection();
@@ -444,14 +437,29 @@ client.on('guildMemberAdd', async member => {
 
 client.login(token);
 
+const http = require('http');
+const express = require('express');
+const app = express();
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
+// setInterval(() => {
+//   http.get(`http://127.0.0.1:${process.env.PORT}/`);
+// }, 10000);
+
 setInterval(() => {
-  http.get(`http://relic-ext.onrender.com/`);
-}, 280000);
+	fetch(`https://relic-ext.onrender.com/`, {
+		method: "get",
+		headers: {
+			"Host": `relic-ext.onrender.com`,
+			"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)",
+			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+			"Accept-Language": "en-us,en;q=0.5",
+		}
+	});
+  }, 280000);
 
 
 
